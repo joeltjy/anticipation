094897662118dbddb705bab271e968fedc8ee8a0
Joel <joeltjy1@mit.edu>
2025-04-10 21:52:20 -0400

    Forgot to add the correct version

--- train/train_amt.py
+++ train/train_amt.py
@@ -5,6 +5,10 @@ import sys
 
 from torch.optim import AdamW
 from transformers import AutoModelForCausalLM, AutoTokenizer, TrainingArguments, Trainer, GPT2LMHeadModel
+from transformers.trainer_callback import TrainerCallback
+sys.path.append(os.path.abspath(".."))
+from anticipation.sample import generate
+from anticipation.convert import events_to_midi
 
 from maestro_dataset import MaestroDataset
 
@@ -17,18 +21,22 @@ LR = 6E-4
 VELOCITY_OFFSET = 55028
 VOCAB_SIZE = VELOCITY_OFFSET + 256
 
+SAVE_STEPS = 1
+# Custom callback class
+class CustomCallback(TrainerCallback):
+    def __init__(self, custom_function, interval_steps=SAVE_STEPS):
+        self.custom_function = custom_function
+        self.interval_steps = interval_steps
+        
+    def on_step_end(self, args, state, control, **kwargs):
+        if state.global_step % self.interval_steps == 0:
+            self.custom_function(state.global_step, kwargs.get('model', None))
 
 if __name__ == "__main__":
     # Load model
-    tokenizer = AutoTokenizer.from_pretrained(GPT2_MODEL_NAME)
     model = AutoModelForCausalLM.from_pretrained(GPT2_MODEL_NAME)
     
-    new_tokens = tokenizer.add_tokens(range(VELOCITY_OFFSET, VOCAB_SIZE))
-    print("added tokens:", new_tokens[:5])
-
     model.resize_token_embeddings(VOCAB_SIZE)
-
-    tokenizer.save_pretrained("velocity_amt_tokenizer")
     model.save_pretrained("velocity_amt_model")
     
     print("total trainable params:", sum(p.numel() for p in model.parameters() if p.requires_grad))
@@ -56,11 +64,11 @@ if __name__ == "__main__":
         warmup_steps=200,
         lr_scheduler_type="cosine",
         max_steps=5000,
-        save_steps=500,
+        save_steps=SAVE_STEPS,
         logging_dir="./logs",
         eval_steps=200,
         logging_steps=10,
-        bf16=True,  # Enable mixed precision
+        bf16=True,
         report_to="wandb",
         run_name="velocity-amt-1",
         dataloader_num_workers=4,
@@ -68,8 +76,18 @@ if __name__ == "__main__":
         evaluation_strategy="steps",
         gradient_accumulation_steps=4,
         save_safetensors=False,
-        # eval_on_start=True,
     )
+    
+    def generate_sample_function(global_step, model, num_samples=5):
+
+        print(f"Generating {num_samples} samples at step {global_step}")
+
+        for i in range(num_samples):
+            sample = generate(model, start_time=0, end_time=20, top_p=.98, include_velocity=True)
+            mid = events_to_midi(sample, include_velocity=True)
+            mid.save(f'checkpoint-{global_step}-sample-{i}-velocity.mid')
+        
+        print(f"Generated {num_samples} samples at step {global_step}")
 
     # Trainer
     trainer = Trainer(
@@ -78,7 +96,8 @@ if __name__ == "__main__":
         train_dataset=ds_train,
         eval_dataset=ds_valid,
         optimizers=(optimizer, None),
+        callbacks=[CustomCallback(generate_sample_function, interval_steps=SAVE_STEPS)]
     )
-
+    
     # Train
     trainer.train()
